909d340c27f88dd5ce9d608ed9edb9dbf45ef6f2
Cole Turner <coleski1@gmail.com>
2018-02-12 23:14:08 -0500

    Initial Commit

---
+++ MultiThreading and Network Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiThreading and Network Practice
+//
+//  Created by Cole Turner on 2/12/18.
+//  Copyright © 2018 Cole Turner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
